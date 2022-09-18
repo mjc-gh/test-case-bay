a088e4dfc7059857fc26c2a929b519aabaeee243
Michael Coyne <mikeycgto@gmail.com>
2022-09-18 17:13:49 +0000

    chore: more updates for outbound mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,11 +67,11 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'test-case-bay.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'yourapp.heroku.com',
+    :port           => ENV['MAIL_SMTP_PORT'],
+    :address        => ENV['MAIL_SMTP_SERVER'],
+    :user_name      => ENV['MAIL_SMTP_LOGIN'],
+    :password       => ENV['MAIL_SMTP_PASSWORD'],
+    :domain         => 'mjcblog.net',
     :authentication => :plain,
   }
 
